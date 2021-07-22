bdb2c057e54c13f2cf90d63da7fabe558e790c4c
Marina Monsivais <minamonsi@me.com>
2021-07-21 22:13:56 -0500

    Installed dependencies & organized product images

---
+++ use-my-tech-app/src/components/ItemList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from 'react-bootstrap';
+import '../App.css';
+
+const ItemList = props => {
+    return(
+        <div className="tech-item">
+            <Card style={{ width: '18rem'}}>
+                <Card.Img variant='top' height='200' src=''/>
+
+            </Card>
+
+        </div>
+    )
+}
