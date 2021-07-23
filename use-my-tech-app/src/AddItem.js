47a0437b82438873f10e01a0bd6295af0cd6e755
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-07-22 21:17:17 -0500

    added new file for adding items for renters

---
+++ use-my-tech-app/src/AddItem.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+function AddItem() {
+	return (
+		<div>
+			hello
+		</div>
+	)
+}
+
+export default AddItem
--- use-my-tech-app/src/App.js
+++ use-my-tech-app/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import LoginPage from './LoginPage'
+import AddItem from './AddItem'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <LoginPage />
+        <LoginPage />
+        <AddItem />
       </header>
       
     </div>
