c5f0ec74d90f094ac7fa371da111af14677b55bf
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-07-22 21:45:34 -0500

    form is complete, now setting up state

--- use-my-tech-app/src/AddItem.js
+++ use-my-tech-app/src/AddItem.js
@@ -1,9 +1,51 @@
 import React, { useState } from 'react'
 
 function AddItem() {
+
+	const handleChange = (e) => {
+	
+	}
+
 	return (
 		<div>
-			hello
+			<form>
+				<label htmlFor="name">Item Name
+					<input
+						type="text"
+						name="name"
+						value=""
+						onChange={handleChange}
+					/>
+				</label>
+				<label htmlFor="category">Category
+					<select
+						name="category"
+						value=""
+						onChange={handleChange}
+					>
+						<option>--select one--</option>
+						<option>Computer</option>
+						<option>Camera</option>
+						<option>Sound Equipment</option>
+						<option>Other</option>
+					</select>
+				</label>
+				<label htmlFor="description"> Description
+					<textarea
+						name="description"
+						value=""
+						onChange={handleChange}
+					>
+
+					</textarea>
+				</label>
+				<label htmlFor="pictures">Add Pictures
+					<input
+						type="file"
+						onChange={handleChange}/>
+				</label>
+			</form>
+
 		</div>
 	)
 }
