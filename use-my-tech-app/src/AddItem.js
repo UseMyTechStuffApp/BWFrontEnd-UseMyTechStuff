d452b8b019eeb7a60888d955c5b35b29e60fb3a6
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-07-22 22:29:23 -0500

    added a schema to the form

--- use-my-tech-app/src/AddItem.js
+++ use-my-tech-app/src/AddItem.js
@@ -1,30 +1,54 @@
 import React, { useState } from 'react'
+import * as yup from 'yup'
+
 
 function AddItem() {
 
+//both functions below handle the state both of errors and the form
 	const [formState, setFormState] = useState({
 		name: '',
 		category: '',
 		description: '',
 		file: ''
-		
-
 	})
 
+	const [errors, setErrors] = useState({
+		name: '',
+		category: '',
+		description: '',
+		file: ''
+	})
+
+//this handles the validation of the form
+	const formSchema = yup.object().shape({
+		name: yup.string().required(),
+		category: yup.string().oneOf(["Computer", "Camera", "Sound", "Other"]).required("Please Select an option"),
+		description: yup.string().length(150).required("Please provide as much detail as possible"),
+		file: yup.mixed()
+		//idk how to do the file, i just figured it was a cool idea to add, even if it doesnt "work"
+	})
+
+	
+
+
+//both functions below handle all changes made
 	const handleChange = (e) => {
 		e.persist()
 		const formData = {
 			...formState,
-			[e.target.name]: e.target.value		
-		}
+			[e.target.name]: e.target.value
+		};
 		setFormState(formData)
-			
-		}
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+	}
 	
 
 	return (
 		<div>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<label htmlFor="name">Item Name
 					<input
 						type="text"
@@ -62,6 +86,8 @@ function AddItem() {
 						value={formState.file}
 						onChange={handleChange}/>
 				</label>
+				<button type="submit">Submit</button>
+
 			</form>
 
 		</div>
