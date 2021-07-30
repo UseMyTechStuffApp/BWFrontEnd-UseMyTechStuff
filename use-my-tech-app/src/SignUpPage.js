e283c4e894e9507e0b35b84a27009044153a9061
Stephen Handy <a.king.life@pm.me>
2021-07-29 21:39:57 -0400

    added breaks

--- use-my-tech-app/src/SignUpPage.js
+++ use-my-tech-app/src/SignUpPage.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import * as yup from 'yup'
+// Reactstrap styling:
+import { ButtonToggle, Form, FormGroup, Label, Input } from 'reactstrap'
 
 
 function SignUpPage() {
@@ -74,64 +76,80 @@ function SignUpPage() {
 
 	return (
 		<div className='SignUpPage'>
-			<form onSubmit={formSubmit}>
-				<label htmlFor="name"> Name:
-					<input
-						type="text"
-						name="name"
-						value={formState.name}
-						id="name"
-						onChange={onChangeHandle}
+			<Form onSubmit={formSubmit}>
+				<FormGroup>
+					<Label htmlFor="name"> Name:
+						<Input
+							type="text"
+							name="name"
+							value={formState.name}
+							id="name"
+							onChange={onChangeHandle}
+							/>
+							{errors.name.length > 0 ? <p>{errors.name}</p> : null}
+							
+					</Label>	
+				</FormGroup>
+				<FormGroup>
+					<Label htmlFor="email"> Email:
+						<Input
+							type="email"
+							name="email"
+							value={formState.email}
+							id="email"
+							onChange={onChangeHandle}
 						/>
-						{errors.name.length > 0 ? <p>{errors.name}</p> : null}
-						
-				</label>
-				<label htmlFor="email"> Email:
-					<input
-						type="email"
-						name="email"
-						value={formState.email}
-						id="email"
-						onChange={onChangeHandle}
-					/>
-						{errors.email.length > 0 ? <p>{errors.email}</p> : null}
-						
-				</label>
-				<label htmlFor="password"> Password:
-					<input
-						type="password"
-						name="password"
-						value={formState.password}
-						id="password"
-						onChange={onChangeHandle}
-					/>
-						{errors.password.length > 0 ? <p>{errors.password}</p> : null}
-						
-				</label>
-				<label htmlFor="confirm"> ConfirmPassword:
-					<input
-						type="password"
-						name="confirm"
-						value={formState.confirm}
-						id="confirm"
-						onChange={onChangeHandle}
-					/>
-						{formState.confirm.match(formState.password) ? null : <p>{errors.confirm}</p>}
-						
-				</label>
-				<label htmlFor="terms"> Terms & Service:
-					<input
-						type="checkbox"
-						name="terms"
-						checked={formState.terms}
-						id="confirms"
-						onChange={onChangeHandle}
-					/>
-						
-						
-				</label>
-				<button type="submit" disabled={buttonDisabled}>Enroll</button>
-			</form>
+							{errors.email.length > 0 ? <p>{errors.email}</p> : null}
+							
+					</Label>
+				</FormGroup>
+				<br></br>
+				<FormGroup>
+					<Label htmlFor="password"> Password:
+						<Input
+							type="password"
+							name="password"
+							value={formState.password}
+							id="password"
+							onChange={onChangeHandle}
+						/>
+							{errors.password.length > 0 ? <p>{errors.password}</p> : null}
+							
+					</Label>
+				</FormGroup>
+				<FormGroup>
+					<Label htmlFor="confirm"> Confirm Password:
+						<Input
+							type="password"
+							name="confirm"
+							value={formState.confirm}
+							id="confirm"
+							onChange={onChangeHandle}
+						/>
+							{formState.confirm.match(formState.password) ? null : <p>{errors.confirm}</p>}
+							
+					</Label>
+				</FormGroup>
+				<br></br>
+				<FormGroup>
+					<Label htmlFor="terms"> Terms & Service:
+						<Input
+							type="checkbox"
+							name="terms"
+							checked={formState.terms}
+							id="confirms"
+							onChange={onChangeHandle}
+						/>
+							
+							
+					</Label>
+				</FormGroup>
+				<br></br>
+				
+				<FormGroup>
+					<ButtonToggle type="submit" disabled={buttonDisabled}>Enroll</ButtonToggle>
+				</FormGroup>
+			</Form>
 		</div>
 	)
 }
